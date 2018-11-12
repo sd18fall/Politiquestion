f8649e2370f59849c8857ced4681c0f6153ac5d3
bwolfe1 <bwolfe1@babson.edu>
2018-11-12 12:01:43 -0500

    more solid header

--- WebApp/.ipynb_checkpoints/page3-checkpoint.py
+++ WebApp/.ipynb_checkpoints/page3-checkpoint.py
@@ -1,7 +1,11 @@
 """ This file is the web app. Right now it is a hard coded version. 
 This will eventually reference the functions in order to display user information. 
 This uses HTML files Results1, ZIPquestionaire1, and zipcode1 as the pages of the webapp. 
+Results1.html displays your vote on an issue, the reps vote on the issue, and your similarity score. 
+ZIPquestionaire1.html displays the form that has bills(1 question rn) that the user "votes" on. 
+zipcode1.html lets the user input their zip code in order to find their rep. 
 """
+
 from flask import Flask
 app = Flask(__name__)
 
