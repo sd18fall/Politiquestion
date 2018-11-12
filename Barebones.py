e66e55407babc261ba0da7861b91c0cdc3b70466
Rocky <rgulassa1@babson.edu>
2018-11-12 10:01:13 -0500

    more structure

--- Barebones.py
+++ Barebones.py
@@ -1,8 +1,34 @@
 from urllib.request import urlopen
 import json
 from pprint import pprint
-url = "https://maps.googleapis.com/maps/api/geocode/json?address=Fenway%20Park"
-f = urlopen(url)
-response_text = f.read()
-response_data = json.loads(str(response_text, "utf-8"))
-pprint(response_data)
+
+def get_json(url):
+    """Given a properly formatted URL for a JSON web API request, return
+    a Python JSON object containing the response to that request.
+    """
+    f = urlopen(url)
+    response_text = f.read()
+    response_data = json.loads(str(response_text, "utf-8"))
+    return response_data
+
+def get_rep(zipcode):
+    """Given a zipcode, can look up the user's representatives using an API
+    """
+    #url=
+    #data=get_json(url)
+    pass
+
+def get_votes(rep):
+    """Given a representative, uses APIs to get their voting history
+    """
+    pass
+
+def vote_to_opinion(votes):
+    """Coverts voting data into a form that can be computed
+    """
+    pass
+
+def compare_opinions(user, rep):
+    """Generates results based on comparing the opinions with weight to user priorities
+    """
+    pass
