2e31cc453986120427f639c974ffd8e001ec1d16
Wolfe <bwolfe1@babson.edu>
2018-12-09 13:01:21 -0500

    finished iterations description on story webpage

--- WebApp/.ipynb_checkpoints/Barebones-checkpoint.py
+++ WebApp/.ipynb_checkpoints/Barebones-checkpoint.py
@@ -2,40 +2,24 @@
 information to the user and to calculate similarity to representative votes.
 """
 import requests
-#import bills
-from bills import Bill
+import bills
 from urllib.request import urlopen
 import json
 from pprint import pprint
+from bills import Bill
 reps=[]
+webpages=[]
 
-# class Bill(object):
-#     def __init__(self, name, ID, congressnum=0):
-#         self.name=name
-#         self.ID=ID
-#         self.congressnum = congressnum
-#         self.rep_vote='Yes'
-#         self.user_vote=0
-#         self.user_preference=1 #scale
-#         self.description = self.get_description()
-#     def does_agree(self):
-#         if self.user_vote==self.rep_vote:
-#             return 1
-#         else:
-#             return 0
-#     def get_description(self):
-#         headers = {"X-API-Key": "a3Kt3J22sEpWhvLjXTrtWf4V560B8XExhkeOmMkD"}
-#         url = "https://api.propublica.org/congress/v1/"+self.congressnum+"/bills/"+self.ID+".json"
-#         r = requests.get(url, headers=headers)
-#         data = r.json()
-#         description = data['results'][0]['summary']
-#         return data['results'][0]['summary']
-
+"""Essential Info for API Bill Lookup"""
 comparing_votes=[
-Bill('North American Energy Security and Infrastructure Act of 2016', 's2012', '114', '2', '54'),
-Bill('Child Interstate Abortion Notification Act', 's403', '109', '2', '216'),
-Bill('Defense of Marriage Act', 'hr3396', '104', '2', '280'),
-Bill('Patient Protection and Affordable Care Act', 'hr3590', '111', '1', '396')]
+Bill('Child Interstate Abortion Notification Act', 'https://www.congress.gov/bill/109th-congress/senate-bill/403?q=%7B', 's403', '109', '2', '216', "Should it be a crime to knowingly transport a minor across a state line to obtain an abortion without satisfying a parental involvement law in the minor's resident state?"),
+Bill('North American Energy Security and Infrastructure Act of 2016', 'https://www.congress.gov/bill/114th-congress/senate-bill/2012?q=%7B', 's2012', '114', '2', '54', "This bill is about stuff"),
+Bill('Defense of Marriage Act', 'https://www.congress.gov/bill/104th-congress/house-bill/3396', 'hr3396', '104', '2', '280', "This bill is about stuff"),
+Bill('Patient Protection and Affordable Care Act', 'https://www.congress.gov/bill/111th-congress/house-bill/3590?q=%7B%22search%22%3A%5B', 'hr3590', '111', '1', '396', "This bill is about stuff"),
+Bill('Support for Patients and Communities Act', 'https://www.congress.gov/bill/115th-congress/house-bill/6?q=%7B', 'hr6', '115', '2', '210', "This bill is about stuff"),
+Bill('Tax Cuts and Jobs Act', 'https://www.congress.gov/bill/115th-congress/house-bill/1?q=%7B', 'hr1', '115', '1', '323', "This bill is about stuff"),
+Bill('Economic Growth, Regulatory Relief, and Consumer Protection Act', 'https://www.congress.gov/bill/115th-congress/senate-bill/2155', 's2155', '115', '2', '54', "This bill is about stuff"),
+Bill("Countering America's Adversaries Through Sanctions Act",'https://www.congress.gov/bill/115th-congress/house-bill/3364', 'hr3364', '115', '1', '175', "This bill is about stuff")]
 
 def get_json(url):
     """Given a properly formatted URL for a JSON web API request, return
@@ -47,36 +31,42 @@ def get_json(url):
     return response_data
 
 def get_rep(zipcode):
-    """Given a zipcode, can look up the user's representatives using an API
-    """
+    """Given a zipcode, can look up the user's representatives using an API while also grabbing their website url"""
     url= "https://www.googleapis.com/civicinfo/v2/representatives?filter[role][legislatorUpperBody]&address="+zipcode+"&key=AIzaSyB9AuQfeJ2TRNSfE8GEHyBfwpgKaISJ7WI"
     data=get_json(url)
-    i=0 #skip prez and vice-prez
-    reps=[]
-    while i <4:
-        repyboi=rep(data["officials"][i]['name'])
+    i=2
+    while i <4: #positions 2-4 are the senators
+        repyboi=data["officials"][i]['name']
+        global reps
         reps.append(repyboi)
+        website=data["officials"][i]['urls'][0] #get the reps website so the user can reach out
+        global webpages
+        webpages.append(website)
         i+=1
     return reps
 
 def compare_opinions(rep):
-    """Generates results based on comparing the opinions with weight to user priorities
-    """
-    passion=4
+    """Generates numeric results based on comparing the opinions with weight to user priorities"""
+    passion=0
     #for Bill in comparing_votes:
         #passion+=Bill.user_preference #get the total perference votes to guage relative preference
-    reps=["Not Werking", "API Sucks"]
     similarity=0
     agree=0
     for Bill in comparing_votes:
         if rep==1:
-            if Bill.rep1_vote==Bill.user_vote:
-                agree+=1
+            if Bill.rep1_vote!='No Vote' and Bill.rep1_vote!='Not Voting':
+                passion+=1
+                if Bill.rep1_vote==Bill.user_vote:
+                    agree+=1
         elif rep==2:
-            if Bill.rep2_vote==Bill.user_vote:
-                agree+=1
+            if Bill.rep2_vote!='No Vote' and Bill.rep2_vote!='Not Voting':
+                passion+=1
+                if Bill.rep2_vote==Bill.user_vote:
+                    agree+=1
+    if passion==0:
+        passion=1
     similarity=agree/passion# use later*(Bill.user_preference/passion) #multiply relative preference by binary of agreement
-    return "Similarity score of "+str(similarity*100)+" with representative "+ reps[1]
+    return "Similarity score of "+str(similarity*100)+" % with representative "+ reps[rep-1]
 
     """Generates results based on comparing the opinions with weight to user priorities
 
@@ -89,43 +79,45 @@ def compare_opinions(rep):
     return "Similarity score of "+str(similarity*100)[:5]+"%" #turns score into percentage with 2 past decimal"""
 
 def give_me_things():
-    """Turns info into format for HTML"""
+    """Turns the question info into format for HTML"""
     descriptions=[]
     i=0
     while i< len(comparing_votes):
-        descriptions.append(comparing_votes[i].name+': '+comparing_votes[i].description)
+        descriptions.append(comparing_votes[i].name+': '+comparing_votes[i].question) #question is a shortened version of API info that get_description would output, we decided the API descriptions were too long
         i+=1
     return descriptions
 
 """USER INPUT"""
 
 def get_user_answers(answers):
+    """takes a list of answers and puts them into the attributes of the bills"""
     i=0
     while i< len(answers):
         comparing_votes[i].user_vote=answers[i]
         i+=1
 
 def get_rep_answers():
-    """Turns info into format for HTML"""
-    rep1_answers=["Yes", "Yes", "Yes"]
-    rep2_answers=["No","No","No"]
-    #for Bill in comparing_votes:
-        #Bill.get_vote() #API calls
-        #rep1_answers.append(Bill.rep1_vote)
-        #rep2_answers.append(Bill.rep2_vote)
-    #return [rep1_answers, rep2_answers]
-    return [["Yes", "Yes", "Yes", "Yes"],["No","No","No", "No"]]
+    """Gathers the representatives votes from the attributes of the Bill objects into lists that Flask can use"""
+    representatives=reps
+    rep1_answers=[]
+    rep2_answers=[]
+    print(representatives)
+    for Bill in comparing_votes:
+        Bill.get_vote(representatives) #API calls
+        rep1_answers.append(Bill.rep1_vote)
+        rep2_answers.append(Bill.rep2_vote)
+    print (rep1_answers, " ", rep2_answers)
+    return [rep1_answers, rep2_answers]
+    #return [["Yes", "Yes", "Yes", "Yes"],["No","No","No", "No"]]
 
-"""def get_rep_answers():
-    Turns info into format for HTML
-    answers=['Yes', 'Yes', 'Yes'] #Insert API reference
-    i=0
-    for i in comparing_votes:
-        answers.append(i.rep_vote)
-    return answers"""
+def get_links():
+    """Grabs links from Bill object attributes and turns them into a list that Flask can use"""
+    links=[]
+    for Bill in comparing_votes:
+        links.append(Bill.link)
+    return links
 
-
-""" This is Main Page Code"""
+""" This is Main Page Code / all of our WebApp functionality"""
 from flask import Flask
 app = Flask(__name__)
 
@@ -138,19 +130,18 @@ ZIPcode=0
 def zipentry():
     return render_template('zipcode3.html')
 
-@app.route('/ZIP', methods =['POST', 'GET'])
+@app.route('/ZIP', methods =['POST'])
 def ZIP():
-    Zip = request.form['ZIP']
-    global ZIPcode
-    ZIPcode = Zip
-    #global reps
-    #reps=get_rep(ZIPcode)
+    Zip=""
+    Zip= request.form['ZIP']
+    reps=get_rep(Zip)
     return redirect(url_for('questions'))
 
-@app.route('/questions', methods = ['POST'])
+@app.route('/questions', methods = ['POST', 'GET'])
 def questions():
     Votes= give_me_things()
-    return render_template('ZIPquestionaire2.html', Vote1=Votes[0], Vote2=Votes[1], Vote3=Votes[2], Vote4=Votes[3])
+    links= get_links()
+    return render_template('ZIPquestionaire2.html', Vote1=Votes[0], Vote2=Votes[1], Vote3=Votes[2], Vote4=Votes[3], Vote5=Votes[4], Vote6=Votes[5], Vote7=Votes[6], Vote8=Votes[7], link1=links[0], link2=links[1], link3=links[2], link4=links[3], link5=links[4], link6=links[5], link7=links[6], link8=links[7])
 
 @app.route('/results', methods = ['POST'])
 def results():
@@ -158,17 +149,21 @@ def results():
     Votes= give_me_things()
     answers=[]
     i = 1
-    #while i < len(Votes):
-        #answers.append(request.form['Q'+ str(i)])
     answers.append(request.form['Q1'])
     answers.append(request.form['Q2'])
     answers.append(request.form['Q3'])
     answers.append(request.form['Q4'])
+    answers.append(request.form['Q5'])
+    answers.append(request.form['Q6'])
+    answers.append(request.form['Q7'])
+    answers.append(request.form['Q8'])
+
     get_user_answers(answers)
     rep_answers = get_rep_answers()
-    Score= compare_opinions(1)
-
-    return render_template('Results2.html', Score=Score, UserScore1=answers[0], Rep1Score1=rep_answers[0][0], Rep1Score2=rep_answers[0][1], Rep1Score3=rep_answers[0][2], Rep1Score4=rep_answers[1][3], UserScore2=answers[1], UserScore3=answers[2], UserScore4=answers[3], Rep2Score1=rep_answers[1][0], Rep2Score2=rep_answers[1][1], Rep2Score3=rep_answers[1][2], Rep2Score4=rep_answers[1][3], Vote1=Votes[0], Vote2=Votes[1], Vote3=Votes[2], Vote4=Votes[3])
+    Score1= compare_opinions(1)
+    Score2= compare_opinions(2)
+    return render_template('Results2.html', Score1=Score1, Score2=Score2, UserScore1=answers[0], Rep1Score1=rep_answers[0][0], Rep1Score2=rep_answers[0][1], Rep1Score3=rep_answers[0][2], Rep1Score4=rep_answers[0][3], Rep1Score5=rep_answers[0][4], Rep1Score6=rep_answers[0][5], Rep1Score7=rep_answers[0][6], Rep1Score8=rep_answers[0][7], UserScore2=answers[1], UserScore3=answers[2], UserScore4=answers[3], UserScore5=answers[4], UserScore6=answers[5], UserScore7=answers[6], UserScore8=answers[7], Rep2Score1=rep_answers[1][0], Rep2Score2=rep_answers[1][1], Rep2Score3=rep_answers[1][2],  Rep2Score4=rep_answers[1][3], Rep2Score5=rep_answers[1][4], Rep2Score6=rep_answers[1][5], Rep2Score7=rep_answers[1][6], Rep2Score8=rep_answers[1][7], Vote1=Votes[0], Vote2=Votes[1], Vote3=Votes[2], Vote4=Votes[3], Vote5=Votes[4], Vote6=Votes[5], Vote7=Votes[6], Vote8=Votes[7], Rep1Page=webpages[0], Rep2Page=webpages[1], Rep1Name=reps[0], Rep2Name=reps[1])
 
 if __name__ == '__main__':
     app.run()
+    #print (get_rep('02457'))
--- docs/Story.md
+++ docs/Story.md
@@ -1,12 +1,25 @@
 # Introduction to the problem we are trying to solve
-We are Politiquestion. We have a survey that will help you see how well your senators are representing YOUR views. 
 
-We are trying to solve the problem that often what politicians say doesn't line up with what they vote. We want the common voter to be able to go on our web app, and find out what their senators are actually doing, and how well it aligns with their viewpoint. 
+We are Politiquestion. We have a survey that will help you see how well your senators are representing YOUR views.
+
+We are trying to solve the problem that often what politicians say doesn't line up with what they vote. We want the common voter to be able to go on our web app, and find out what their senators are actually doing, and how well it aligns with their viewpoint.
 
 # How we got the inspiration for this project
 
-We knew we wanted to do something related to social justice, and as we were starting this project the midterm elections were coming up. We realized that we could make an impact in future elections be creating this web app. 
+We knew we wanted to do something related to social justice, and as we were starting this project the midterm elections were coming up. We realized that we could make an impact in future elections be creating this web app.
 
 # Walkthrough of versions and iterations
+
+In our first iteration we had separate files and functionality for the web app and the API pulls. The API pulls worked on gathering data and using that data to get the next API information. While the web app was not connected to that information, and just was created to have pre-filled templates display fake data. We did this as a first step in order to let the two pieces be developed simultaneously.
+
+In our next iteration we tested the functionality of several main functions in Barebones.py but inputted fake data, in order to give the API coding more time. We used this iteration to add variables into the web app templates and to create more interactions in between the two pieces that were previously disconnected.
+
+The third iteration was when we connected the APIs. We started by connecting the descriptions API. this let us have questions to display. We then got the votes of the senators from the API, but still did not have the ZIP code reading and finding senators from that functionality. Instead we used hard coded senator names to search for the votes.
+
+Next, we connected the ZIP code API, in order to find who the user's senators were based on the ZIP code entered. This proved to be a challenge because the resulting output of names did not always match the API database of votes. In order to counter this we wrote a function that compares only the last name of the senators to each other, with no spaces or other words. This allowed us to more effectively find senator's votes.
+
+After that, we realized that the descriptions that were being pulled from the API were pages long and we considered either just using the first 5 sentences or writing our own questions based on the bills. Instead of either option we chose middle ground. We added links, to the website where we had been pulling the descriptions from, in our questionnaire, and created our own questions for the user to answer. 
+
+Last, we decided to add an API call that got the senators personal websites and displayed that information in the results page. This was added so that the user could get more information about their senators and contact them from there if they so desired.
 
 # The final product
